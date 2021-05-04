ac1d20e5a715360ab63e5099c585d9a89ef5cd46
marlenyg <marleny.garzon@wsu.edu>
2021-05-04 14:00:12 -0700

    Redo

--- Source1.cpp
+++ Source1.cpp
@@ -1,43 +1,118 @@
-#include<iostream>
-#include<semaphore.h>
-#include<string>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
 
+#define N 5
+#define THINKING 2
+#define HUNGRY 1
+#define EATING 0
+#define LEFT (phnum + 4) % N
+#define RIGHT (phnum + 1) % N
 
+int state[N];
+int phil[N] = { 0, 1, 2, 3, 4 };
 
-using namespace std;
+sem_t mutex;
+sem_t S[N];
 
-class Philosopher: public thread
+void test(int phnum)
 {
-public:
-	State state;
-	int total;
-	int id;
-	string name;
+	if (state[phnum] == HUNGRY
+		&& state[LEFT] != EATING
+		&& state[RIGHT] != EATING) {
+	
+		state[phnum] = EATING;
 
-	Chopstick  left;
-	Chopstick right;
+		sleep(2);
 
-	Syncro* syncro;
-	float thinking;
-	float hungry;
-	float eat;
+		printf("Philosopher %d takes fork %d and %d\n",
+			phnum + 1, LEFT + 1, phnum + 1);
 
-	Philosopher(string n, int i, Syncro* s, int t);
-};
+		printf("Philosopher %d is Eating\n", phnum + 1);
 
-Philosopher::Philosopher(int s, int i) {
-	state = s;
-	id = i;
+	
+		sem_post(&S[phnum]);
+	}
 }
 
-class Syncro
+// take up chopsticks
+void take_fork(int phnum)
 {
-public:
-	int index;
 
-	Syncro(int n);
-	void putDownChopstick(int ii);
-	void pickUpChopstick(int i);
-	
-	static int state[5];
-};
+	sem_wait(&mutex);
+
+	// state that hungry
+	state[phnum] = HUNGRY;
+
+	printf("Philosopher %d is Hungry\n", phnum + 1);
+
+	// eat if neighbours are not eating
+	test(phnum);
+
+	sem_post(&mutex);
+
+	// if unable to eat wait to be signalled
+	sem_wait(&S[phnum]);
+
+	sleep(1);
+}
+
+// put down chopsticks
+void put_fork(int phnum)
+{
+
+	sem_wait(&mutex);
+
+	state[phnum] = THINKING;
+
+	printf("Philosopher %d putting fork %d and %d down\n",
+		phnum + 1, LEFT + 1, phnum + 1);
+	printf("Philosopher %d is thinking\n", phnum + 1);
+
+	test(LEFT);
+	test(RIGHT);
+
+	sem_post(&mutex);
+}
+
+void* philospher(void* num)
+{
+
+	while (1) {
+
+		int* i = num;
+
+		sleep(1);
+
+		take_fork(*i);
+
+		sleep(0);
+
+		put_fork(*i);
+	}
+}
+
+int main()
+{
+
+	int i;
+	pthread_t thread_id[N];
+
+	sem_init(&mutex, 0, 1);
+
+	for (i = 0; i < N; i++)
+
+		sem_init(&S[i], 0, 0);
+
+	for (i = 0; i < N; i++) {
+
+		pthread_create(&thread_id[i], NULL,
+			philospher, &phil[i]);
+
+		printf("Philosopher %d is thinking\n", i + 1);
+	}
+
+	for (i = 0; i < N; i++)
+
+		pthread_join(thread_id[i], NULL);
+}
